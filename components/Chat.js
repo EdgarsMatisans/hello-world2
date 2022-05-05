71a9f86b00309aac808c6cd3c00b295018769b88
Edgars Matisans <80711384+EdgarsMatisans@users.noreply.github.com>
2022-05-05 11:07:50 +0200

    5.3
    Work with Jonathan

--- components/Chat.js
+++ components/Chat.js
@@ -2,12 +2,15 @@
 // import "firebase/firestore";
 import React from "react";
 import { View, Text, Button, TextInput, StyleSheet } from "react-native";
-import { GiftedChat, InputToolbar } from "react-native-gifted-chat";
-import { createStackNavigator } from "@react-navigation/stack";
-import * as firebase from "firebase/compat";
-import "firebase/compat/firestore";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import NetInfo from "@react-native-community/netinfo";
+import { GiftedChat } from "react-native-gifted-chat";
+
+// import * as firebase from "firebase/compat";
+// import "firebase/firestore";
+// import * as firebase from "firebase";
+// import "firebase/firestore";
+
+const firebase = require("firebase");
+require("firebase/firestore");
 
 export default class Chat extends React.Component {
   constructor() {
@@ -45,42 +48,8 @@ export default class Chat extends React.Component {
     this.refMsgsUser = null;
   }
 
-  //retrieve messsages asynchronious trough asyncstorage
-  async getMessages() {
-    let messages = "";
-    try {
-      messages = (await AsyncStorage.getItem("messages")) || [];
-      this.setState({
-        messages: JSON.parse(messages),
-      });
-    } catch (error) {
-      console.log(error.message);
-    }
-  }
-  //function to save message in asynstorage
-  async saveMessages() {
-    try {
-      await AsyncStorage.setItem(
-        "messages",
-        JSON.stringify(this.state.messages)
-      );
-    } catch (error) {
-      console.log(error.message);
-    }
-  }
-  //function to delet message in asynstorage
-  async deleteMessages() {
-    try {
-      await AsyncStorage.removeItem("messages");
-      this.setState({
-        messages: [],
-      });
-    } catch (error) {
-      console.log(error.message);
-    }
-  }
-
-  componentDidMount() {
+  // creates the initial welcome message and the system message when component did mount
+  ccomponentDidMount() {
     //  takes the entered username from start.js assigned to a variable "name"
     let name = this.props.route.params.name;
     this.props.navigation.setOptions({ title: name });
@@ -130,97 +99,71 @@ export default class Chat extends React.Component {
       }
     });
   }
-  onCollectionUpdate = (querySnapshot) => {
-    const messages = [];
-    // go through each document
-    querySnapshot.forEach((doc) => {
-      // get the QueryDocumentSnapshot's data
-      let data = doc.data();
-      messages.push({
-        _id: data._id,
-        text: data.text,
-        createdAt: data.createdAt.toDate(),
-        user: {
-          _id: data.user._id,
-          name: data.user.name,
-          avatar: data.user.avatar,
-        },
-        image: data.image,
-        location: data.locationl,
-      });
-    });
-    this.setState({
-      messages: messages,
-    });
 
-    this.saveMessages();
-  };
-
-  addMessages() {
-    // add a new message + user data  to the messages collection
-    const message = this.state.messages[0];
-    this.referenceMessages.add({
-      _id: message._id,
-      text: message.text,
-      createdAt: message.createdAt,
-      user: this.state.user,
-      image: message.image,
-      location: message.location,
-    });
-  }
-
-  //this function adds new chat messages to the state
   onSend(messages = []) {
     this.setState(
       (previousState) => ({
         messages: GiftedChat.append(previousState.messages, messages),
       }),
       () => {
+        this.addMessages();
         this.saveMessages();
       }
     );
   }
 
   componentWillUnmount() {
-    NetInfo.fetch().then((connection) => {
-      if (connection.isConnected) {
-        this.unsubscribe();
-        this.authUnsubscribe();
-      }
-    });
+    // stop listening for changes
+    this.unsubscribe();
+    this.authUnsubscribe();
   }
-  //changes how the chat bar is rendered
-  renderInputToolbar(props) {
-    if (this.state.isConnected == false) {
-    } else {
-      return <InputToolbar {...props} />;
-    }
+
+  /* retrieves the current data in "messages" collection and 
+  stores it in state "messages" to render it in view */
+  onCollectionUpdate = (QuerySnapshot) => {
+    const messages = [];
+    // go through each document
+    QuerySnapshot.forEach((doc) => {
+      // gets the QueryDocumentSnapshot's data
+      let data = doc.data();
+      messages.push({
+        _id: data._id,
+        text: data.text,
+        createdAt: data.createdAt.toDate(),
+        user: data.user,
+      });
+    });
+    this.setState({
+      messages,
+    });
+  };
+
+  addMessages() {
+    const message = this.state.messages[0];
+    // adds new message to the collection
+    this.referenceChatMessages.add({
+      uid: this.state.uid,
+      _id: message._id,
+      text: message.text || "",
+      createdAt: message.createdAt,
+      user: this.state.user,
+    });
   }
 
   render() {
-    // takes the input parameters of background color defined in the start.js component
-
-    let bgColor = this.props.route.params.bgColor;
     return (
-      <View
-        style={{
-          flex: 1,
-          backgroundColor: bgColor,
-        }}
-      >
+      <View style={{ flex: 1 }}>
         <GiftedChat
-          renderInputToolbar={this.renderInputToolbar.bind(this)}
+          renderSystemMessage={this.renderSystemMessage}
+          renderDay={this.renderDay}
           messages={this.state.messages}
           onSend={(messages) => this.onSend(messages)}
-          renderActions={this.renderCustomActions}
           user={{
             _id: this.state.user._id,
             name: this.state.name,
             avatar: this.state.user.avatar,
           }}
         />
-
-        {/* this fixes android keyboard */}
         {Platform.OS === "android" ? (
           <KeyboardAvoidingView behavior="height" />
         ) : null}
